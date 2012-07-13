3a19654676b28cad0260a6e599a3ecfcd5f6aac2
Masahiro Ide <imasahiro9@gmail.com>
2012-07-13 09:53:56 +0900

    fix dict

--- dict.c
+++ dict.c
@@ -14,13 +14,13 @@ static dictmap_t *_dictmap_init(dictmap_t* m, const poolmap_entry_api_t *api)
 {
     int i;
     const size_t allocSize = sizeof(pmap_record_t)*DICTMAP_THRESHOLD;
+    m->api = api;
+    m->records = cast(pmap_record_t *,malloc(allocSize));
     uint64_t *hash_list = (uint64_t *) m->hash_list;
+    m->used_size = 0;
     for (i = 0; i < DICTMAP_THRESHOLD/2; ++i) {
         hash_list[i] = 0;
     }
-    m->api = api;
-    m->records = cast(pmap_record_t *,malloc(allocSize));
-    m->used_size = 0;
     return m;
 }
 
@@ -45,50 +45,50 @@ static inline pmap_record_t *dmap_at(dictmap_t *m, uint32_t idx)
     return (pmap_record_t*)(m->records+idx);
 }
 
-static pmap_status_t _dmap_set(dictmap_t *m, pmap_record_t *rec)
+static pmap_status_t _dmap_set_new(dictmap_t *m, pmap_record_t *rec, int i)
 {
-    int i = 0;
-    pmap_record_t *r;
-    L_head:;
-    for (; i < DICTMAP_THRESHOLD; ++i) {
-        uint32_t hash = m->hash_list[i];
-        if (hash == rec->hash) {
-            goto L_update;
-        }
-        if (hash == 0) {
-            goto L_new;
-        }
-    }
-    assert(0);
-    return -1;
-    L_new:;
-    r = dmap_at(m, i);
+    pmap_record_t *r = dmap_at(m, i);
     m->hash_list[i] = rec->hash;
     dmap_record_copy(r, rec);
     ++m->used_size;
     return POOLMAP_ADDED;
+}
 
-    L_update:;
-    r = dmap_at(m, i);
-    if (!m->api->fcmp(r->k, rec->k)) {
-        goto L_head;
+static pmap_status_t _dmap_set(dictmap_t *m, pmap_record_t *rec)
+{
+    int i;
+    const poolmap_entry_api_t *api = m->api;
+    for (i = 0; i < DICTMAP_THRESHOLD; ++i) {
+        uint32_t hash = m->hash_list[i];
+        if (hash == 0) {
+            return _dmap_set_new(m, rec, i);
+        }
+        else if (hash == rec->hash) {
+            pmap_record_t *r = dmap_at(m, i);
+            if (!api->fcmp(r->k, rec->k)) {
+                continue;
+            }
+            uintptr_t old0 = r->v;
+            uint32_t  old1 = r->v2;
+            api->ffree(r);
+            dmap_record_copy(r, rec);
+            rec->v  = old0;
+            rec->v2 = old1;
+            return POOLMAP_UPDATE;
+        }
     }
-    uintptr_t old0 = r->v;
-    uint32_t  old1 = r->v2;
-    m->api->ffree(r);
-    dmap_record_copy(r, rec);
-    rec->v  = old0;
-    rec->v2 = old1;
-    return POOLMAP_UPDATE;
+    assert(0);
+    return -1;
 }
 
 static pmap_record_t *dmap_get_(dictmap_t *m, uint32_t hash, uintptr_t key)
 {
-    int i = 0;
+    int i;
+    fn_keycmp fcmp = m->api->fcmp;
     for (i = 0; i < DICTMAP_THRESHOLD; ++i) {
         if (hash == m->hash_list[i]) {
             pmap_record_t *r = dmap_at(m, i);
-            if (m->api->fcmp(r->k, key)) {
+            if (fcmp(r->k, key)) {
                 return r;
             }
         }
