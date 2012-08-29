52b634767b31e39233cd0815dec25da2a49bdef2
Masahiro Ide <imasahiro9@gmail.com>
2012-08-29 10:44:49 +0900

    fix test/check_cpp_build.cpp

--- test/check_cpp_build.cpp
+++ test/check_cpp_build.cpp
@@ -1,23 +1,25 @@
-#include "../kjson.c"
-#include "../map.c"
-#include "../stream.c"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../kjson.c"
+#include "../map.c"
+#include "../stream.c"
+
 int main(int argc, char const* argv[])
 {
-    FILE *fp = fopen("test/sample.json", "rb");
-    if (!fp) 
-        fp = fopen("../test/sample.json", "rb");
+#define JSON_FILE "test/simple.json"
+    FILE *fp = fopen(JSON_FILE, "r");
+    if (!fp)
+        fp = fopen("../" JSON_FILE, "r");
     assert(fp != 0);
 
     fseek(fp, 0, SEEK_END);
-    size_t len = (size_t)ftell(fp);
+    size_t len = (size_t) ftell(fp);
     fseek(fp, 0, SEEK_SET);
-    char *json = (char*)calloc(1, len + 1);
-    assert(len == fread(json, 1, len, fp));
-    json[len] = '\0';
+    char *json = (char *) calloc(1, len + 1);
+    size_t readed = fread(json, 1, len, fp);
+    assert(len == readed);
     fclose(fp);
 
     JSON o = parseJSON(json, json + len);
