43bf09bb3a156c8e0d8a50d92c0cc86f8aafca75
Masahiro Ide <imasahiro9@gmail.com>
2012-10-01 08:43:40 +0900

    remove *-inline.h

--- kjson.h
+++ kjson.h
@@ -153,6 +153,147 @@ typedef struct JSONObject_iterator {
 }
 #endif
 
-#include "kjson-inline.h"
+#ifndef LOG2
+#define LOG2(N) ((uint32_t)((sizeof(void*) * 8) - __builtin_clzl(N - 1)))
+#endif
+
+#ifdef __cplusplus
+static inline JSON toJSON(Value v) {
+    JSON json;
+    json.bits = v.bits;
+    return json;
+}
+#else
+#define toJSON(O) ((JSON) O)
+#endif
+
+#ifndef INT32_MAX
+#define INT32_MAX        2147483647
+#endif
+
+#ifndef INT32_MIN
+#define INT32_MIN        (-INT32_MAX-1)
+#endif
+
+static inline JSON JSON_parse(const char *str)
+{
+    const char *end = str + strlen(str);
+    return parseJSON(str, end);
+}
+
+/* [Getter API] */
+static inline const char *JSONString_get(JSON json)
+{
+    JSONString *s = toStr(json.val);
+    return s->str;
+}
+
+static inline int32_t JSONInt_get(JSON json)
+{
+    return toInt32(json.val);
+}
+
+static inline double JSONDouble_get(JSON json)
+{
+    return toDouble(json.val);
+}
+
+static inline int JSONBool_get(JSON json)
+{
+    return toBool(json.val);
+}
+
+/* [New API] */
+static inline JSON JSONString_new(const char *s, size_t len)
+{
+    JSONString *o = (JSONString *) MPOOL_ALLOC(sizeof(*o)+len+1);
+    o->str = (const char *) (o+1);
+    o->hashcode = 0;
+    o->length = len;
+    memcpy((char *)o->str, s, len);
+    ((char*)o->str)[len] = 0;
+    return toJSON(ValueS(o));
+}
+
+static inline JSON JSONNull_new()
+{
+    return toJSON(ValueN());
+}
+
+static inline JSON JSONObject_new()
+{
+    JSONObject *o = (JSONObject *) MPOOL_ALLOC(sizeof(*o));
+    kmap_init(&(o->child), 0);
+    return toJSON(ValueO(o));
+}
+
+static inline JSON JSONArray_new()
+{
+    JSONArray *o = (JSONArray *) MPOOL_ALLOC(sizeof(*o));
+    o->length   = 0;
+    o->capacity = 0;
+    o->list   = NULL;
+    return toJSON(ValueA(o));
+}
+
+static inline JSON JSONDouble_new(double val)
+{
+    return toJSON(ValueF(val));
+}
+
+static inline JSON JSONInt_new(int64_t val)
+{
+    if (val > INT32_MAX || val < INT32_MIN) {
+        JSONInt64 *i64 = (JSONInt64 *) MPOOL_ALLOC(sizeof(JSONInt64));
+        i64->val = val;
+        return toJSON(ValueIO(i64));
+    } else {
+        return toJSON(ValueI(val));
+    }
+}
+
+static inline JSON JSONBool_new(bool val)
+{
+    return toJSON(ValueB(val));
+}
+
+static inline unsigned JSON_length(JSON json)
+{
+    assert((JSON_type(json) & 0x3) == 0x1);
+    JSONArray *a = toAry(json.val);
+    return a->length;
+}
+
+static inline JSON JSONArray_get(JSON json, unsigned index)
+{
+    if (JSON_TYPE_CHECK(Array, json)) {
+        JSONArray *a = toAry(json.val);
+        return (a)->list[index];
+    } else {
+        return JSONNull_new();
+    }
+}
+
+static inline int JSONObject_iterator_init(JSONObject_iterator *itr, JSON json)
+{
+    if (!JSON_type(json) ==  JSON_Object)
+        return 0;
+    itr->obj = toObj(json.val);
+    itr->index = 0;
+    return 1;
+}
+
+static inline JSON JSONObject_iterator_next(JSONObject_iterator *itr, JSON *val)
+{
+    JSONObject *o = itr->obj;
+    map_record_t *r;
+    while ((r = kmap_next(&o->child, (kmap_iterator*) itr)) != NULL) {
+        *val = toJSON(ValueP(r->v));
+        return toJSON(ValueS(r->k));
+    }
+    JSON obj; obj.bits = 0;
+    *val = obj;
+    return obj;
+}
 
 #endif /* end of include guard */
--- kmap.h
+++ kmap.h
@@ -83,10 +83,48 @@ struct map_api {
     void (*_dispose)(kmap_t *m);
 };
 
+/* [kmap inline] */
+
+static inline void kmap_dispose(kmap_t *m)
+{
+    m->h.base.api->_dispose(m);
+}
+
+static inline map_record_t *kmap_get(kmap_t *m, struct JSONString *key)
+{
+    return m->h.base.api->_get(m, key);
+}
+
+static inline map_status_t kmap_set(kmap_t *m, struct JSONString *key, uint64_t val)
+{
+    return m->h.base.api->_set(m, key, val);
+}
+
+static inline void kmap_remove(kmap_t *m, struct JSONString *key)
+{
+    return m->h.base.api->_remove(m, key);
+}
+
+static inline map_record_t *kmap_next(kmap_t *m, kmap_iterator *itr)
+{
+    return m->h.base.api->_next(m, itr);
+}
+
+static inline unsigned kmap_size(kmap_t *m)
+{
+    return m->h.used_size;
+}
+
+extern const kmap_api_t DICT;
+extern const kmap_api_t HASH;
+static inline void kmap_init(kmap_t *m, unsigned init)
+{
+    const kmap_api_t *api = (init > DICTMAP_THRESHOLD) ? &HASH:&DICT;
+    m->h.base.api = api;
+    api->_init(m, init);
+}
+
 #ifdef __cplusplus
 }
 #endif
-
-#include "kmap-inline.h"
-
 #endif /* end of include guard */
